974cfb4d5204b1105c58e817608505ed307dc1d5
yamasakura <1969314158@qq.com>
2026-08-29 16:50:54 +0800

    feat:更换根域名

--- src/api/BASE_URL.ts
+++ src/api/BASE_URL.ts
@@ -1,3 +1,3 @@
 // UC（UserCenter 统一用户中心）服务地址：上线前改为 UC 正式域名（如 https://uc.yituliu.cn）
-export const UC_BASE_URL = 'http://127.0.0.1:8080'
-//  export const UC_BASE_URL = 'https://auth.yituliu.cn'
+// export const UC_BASE_URL = 'http://127.0.0.1:8080'
+ export const UC_BASE_URL = 'https://auth.yituliu.cn'
